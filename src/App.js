2445b7c51404b6859cd6e0a60989a42d93435b7f
Zack Horwitz <zackclan@gmail.com>
2019-02-06 23:00:56 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,99 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import sunny from './sunny.jpg';
+import stormy from './stormy.jpg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+
+
+
+class Weather extends Component {
+  toF(temp){
+    return Math.round((temp-273.15) * 9/5 + 32)
   }
+
+  render(){
+    return(
+      this.props.weatherData.map((x) => {
+        return(
+          <div class="weather">
+            <p class="name">{x.name}</p>
+            <p>{this.toF(x.main.temp)}&deg;F</p>
+            <p>{x.weather[0].main}</p>
+          </div>
+        )
+      })        
+    )
+  }
+}
+
+
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      weatherData : [],
+      location: '',
+      value : '',
+      background : sunny
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleKeyPress = this.handleKeyPress.bind(this)
+    this.getWeatherData = this.getWeatherData.bind(this)
+  }
+
+  handleKeyPress(e){
+    if(e.key === "Enter"){
+      let value = this.state.value
+      this.setState({location:value})
+      this.setState({value:''})
+      this.getWeatherData();
+    }
+  }
+
+  handleChange(e){
+    this.setState({value:e.target.value})
+  }
+
+  getWeatherData(){
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&APPID=54e5c316b975fbdbb506569977dee87e`)
+      .then(response => response.json())
+      .then(data => {
+        let skies = data.weather[0].main
+        if(skies === 'Clear'){
+          this.setState({background: sunny})
+        }else{
+          this.setState({background:stormy})
+        }        
+        return this.setState({weatherData: [...this.state.weatherData,data] })
+      }).catch(function(){
+        alert('That city was not found')
+      })
+  }
+
+  getBackground(){
+    if (this.state.weatherData.length > 0){
+    return this.state.weatherData[0].weather[0].main === 'clear' ? this.setState({background:sunny}) : this.setState({background:stormy})
+    } else {return stormy}
+  }
+
+
+  render() {
+    const divStyle = {
+      backgroundImage: 'url(' + this.state.background + ')'
+    }
+    return(
+    <div class='container' style={divStyle}>
+      <div class='app'>
+        <div class='inputField'>
+          <input type='text' placeholder='Pick a city then press enter' value={this.state.value} onChange={this.handleChange} onKeyPress={this.handleKeyPress} />
+        </div>
+        <Weather
+          weatherData={this.state.weatherData}
+        />    
+      </div>
+    </div>
+  )}
 }
 
 export default App;
