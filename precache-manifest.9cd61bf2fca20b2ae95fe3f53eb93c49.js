3a21e180f1d326897fe4277e3e85fb5ec534b193
Zack Horwitz <zackclan@gmail.com>
2019-02-06 23:02:21 -0500

    Updates

---
+++ precache-manifest.9cd61bf2fca20b2ae95fe3f53eb93c49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "227f94b0cc14fb53194f",
+    "url": "/ReactWeather/static/css/main.248a4869.chunk.css"
+  },
+  {
+    "revision": "227f94b0cc14fb53194f",
+    "url": "/ReactWeather/static/js/main.227f94b0.chunk.js"
+  },
+  {
+    "revision": "2bb85aa9bacc144510d6",
+    "url": "/ReactWeather/static/js/1.2bb85aa9.chunk.js"
+  },
+  {
+    "revision": "a2622fa2ad826d6da5df",
+    "url": "/ReactWeather/static/js/runtime~main.a2622fa2.js"
+  },
+  {
+    "revision": "927c829c9e3c7b5bd6b98ddf56d94c32",
+    "url": "/ReactWeather/static/media/sunny.927c829c.jpg"
+  },
+  {
+    "revision": "7ec0277031957245c801bce5d26bb018",
+    "url": "/ReactWeather/static/media/stormy.7ec02770.jpg"
+  },
+  {
+    "revision": "14855badb470e63e8bc7eca18d248e8b",
+    "url": "/ReactWeather/index.html"
+  }
+];
